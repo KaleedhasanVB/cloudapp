212d458bc901b5715bf29681317428cfff649fd0
KaleedhasanVB <113677283+KaleedhasanVB@users.noreply.github.com>
2023-12-05 00:24:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -901,4 +901,4 @@ for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
